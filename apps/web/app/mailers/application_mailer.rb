8b0152ed3f34bb0f8d74a9d9017e70473fb6aefa
Emi <emirn@users.noreply.github.com>
2026-04-29 00:21:20 +0400

    changing default ENV variable for default from email. Email based signup simplified to ask for email address only and fill first name and last name automatically (#8)

--- apps/web/app/mailers/application_mailer.rb
+++ apps/web/app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  # Default from-address. Override by setting MAILER_SENDER in the environment.
-  default from: ENV.fetch("MAILER_SENDER", "noreply@localhost")
+  # Default from-address. Override by setting DEFAULT_FROM_EMAIL in the environment.
+  default from: ENV.fetch("DEFAULT_FROM_EMAIL", "no-reply@localhost")
   layout "mailer"
 end
